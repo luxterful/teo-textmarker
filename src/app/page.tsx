9735c2c33be04ec822c1ac073fdde7f85f89193f
luxterful <derlukasbauer@gmail.com>
2024-11-16 19:15:52 +0100

    adds more processe; refactor page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,58 @@
 "use client";
 
 import processes from "@/app/resources/processes";
+import React from "react";
 import { ChangeEvent, useState } from "react";
+
+function CopyField(props: { fieldId: string }) {
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  function copy() {
+    setShowSuccess(true);
+    navigator.clipboard.writeText(props.fieldId);
+
+    setTimeout(() => {
+      setShowSuccess(false); // Reset animating state
+    }, 300);
+  }
+
+  return (
+    <td
+      className="border p-2 rounded-md cursor-pointer flex gap-2 items-center text-gray-600 hover:text-black truncate overflow-hidden text-sm md:text-base"
+      onClick={copy}
+    >
+      {showSuccess ? (
+        <svg
+          className="w-3.5 h-3.5 inline flex-none"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 16 12"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5.917 5.724 10.5 15 1.5"
+          />
+        </svg>
+      ) : (
+        <svg
+          className="w-3.5 h-3.5 inline flex-none"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 20"
+        >
+          <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+        </svg>
+      )}
+
+      {props.fieldId}
+    </td>
+  );
+}
 
 export default function Home() {
   const [selection, setSelection] = useState(processes[0].title);
@@ -21,7 +72,7 @@ export default function Home() {
   return (
     <div className="container mx-auto px-5">
       <select
-        className="border rounded-md mb-5"
+        className="border rounded-md mb-5 w-full overflow-hidden"
         value={selection}
         onChange={handleChange}
       >
@@ -31,39 +82,31 @@ export default function Home() {
       </select>
 
       <div className={`content-div ${animating ? "fade-out" : "fade-in"}`}>
-        {processes
-          .find((process) => process.title === selection)!
-          .groups.map((group) => (
-            <div className="mb-5" key={group.title}>
-              <span className="text-lg font-bold">{group.title}</span>
-              <table className="table-auto border-separate border-spacing-2">
-                <tbody>
+        <table className="table-fixed border-separate border-spacing-y-1 border-spacing-x-5 w-full">
+          <tbody>
+            {processes
+              .find((process) => process.title === selection)!
+              .groups.map((group) => (
+                <React.Fragment key={group.title}>
+                  <tr
+                    key={group.title}
+                    className="text-lg font-bold md:text-right"
+                  >
+                    <td className="pt-5">{group.title}</td>
+                  </tr>
+
                   {group.fields.map((field) => (
                     <tr key={group.title + field.id}>
-                      <td>{field.title}</td>
-                      <td
-                        className="border p-2 rounded-md cursor-pointer flex gap-2 items-center text-gray-600 hover:text-black"
-                        onClick={async () => {
-                          navigator.clipboard.writeText("");
-                        }}
-                      >
-                        <svg
-                          className="w-3.5 h-3.5 inline "
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 18 20"
-                        >
-                          <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
-                        </svg>
-                        {field.id}
+                      <td className="md:text-right block md:table-cell whitespace-nowrap">
+                        {field.title}
                       </td>
+                      <CopyField fieldId={field.id} />
                     </tr>
                   ))}
-                </tbody>
-              </table>
-            </div>
-          ))}
+                </React.Fragment>
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/app/resources/processes.ts
+++ src/app/resources/processes.ts
@@ -79,4 +79,57 @@ export default [
       groups.discharge_general("Entlassung allgemein"),
     ],
   },
+  {
+    title:
+      "Aufnahmevertrag Sicherheitsverwahrung (Einweisungsvereinbarung Abgabe durch Behörde)",
+    groups: [
+      groups.animal("Tier"),
+      groups.contact("Pfleger", "Pfleger"),
+      groups.shelter("Tierheim"),
+      groups.admissions_general("Aufnahme allgemein"),
+      groups.contact("Überbringer", "Ueberbringer"),
+      groups.contact("Halter", "Halter"),
+    ],
+  },
+  {
+    title:
+      "Entlassung Sicherheitsverwahrung (Sicherheitsverwahrungsrückgabevertrag)",
+    groups: [
+      groups.animal("Tier"),
+      groups.contact("Pfleger", "Pfleger"),
+      groups.shelter("Tierheim"),
+      groups.admissions_general("Aufnahme allgemein"),
+      groups.contact("Überbringer", "Ueberbringer"),
+      groups.contact("Halter", "Halter"),
+      groups.contact("Zahler", "Zahler"),
+      groups.contact(
+        "Nachpruefer (falls vorhanden, sonst leer)",
+        "Nachpruefer"
+      ),
+      groups.discharge_general("Entlassung allgemein"),
+      groups.discharge_security_custody("Entlassung Sicherheitsverwahrung"),
+    ],
+  },
+  {
+    title: "Tod (Bestätigung Ableben)",
+    groups: [
+      groups.animal("Tier"),
+      groups.contact("Pfleger (falls vorhanden, sonst leer)", "Pfleger"),
+      groups.shelter("Tierheim"),
+      groups.discharge_general("Entlassung allgemein"),
+      groups.admissions_general("Aufnahme allgemein"),
+      groups.admissions_boarding(
+        "Aufnahme Pension (falls vorhanden, sonst leer)"
+      ),
+      groups.admissions_found(
+        "Aufnahme Fundtier (falls vorhanden, sonst leer)"
+      ),
+      groups.contact("Halter (falls vorhanden, sonst leer)", "Halter"),
+
+      groups.contact(
+        "Ueberbringer (falls vorhanden, sonst leer)",
+        "Ueberbringer"
+      ),
+    ],
+  },
 ];
